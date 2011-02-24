631d9683671d43ee6278afa2fd5300ead834b458
Robert Porter <rob@Robs-MacBook-2.local>
2011-02-23 18:46:08 -0800

    Fixed InvalidateWindow() on mac.  Added 10.4 support.

--- src/PluginAuto/Mac/PluginWindowMacCarbon.cpp
+++ src/PluginAuto/Mac/PluginWindowMacCarbon.cpp
@@ -75,8 +75,8 @@ uint32_t PluginWindowMacCarbon::getWindowWidth() const {
 }
 
 void PluginWindowMacCarbon::InvalidateWindow() const {
-    FB::Rect pos = getWindowPosition();
-    NPRect r = { pos.top, pos.left, pos.bottom, pos.right };
+
+    NPRect r = { 0, 0, m_height, m_width };
     if (!m_npHost->isMainThread()) {
         m_npHost->ScheduleOnMainThread(m_npHost, boost::bind(&Npapi::NpapiBrowserHost::InvalidateRect2, m_npHost, r));
     } else {
